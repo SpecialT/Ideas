73b4215719deb19559dfee835c3d68741b61b621
Todd Sutter <tsuttert@gmail.com>
2016-03-26 16:41:27 -0500

    added a java program for Tyler to run

---
+++ HiTyler.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class HiTyler {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("\n\nEnter an insult: ");
+		String insult = in.nextLine();
+		if (insult.toLowerCase().equals("no")) {
+			System.out.println("Good. I didn't want you to anyway, Tyler...\n\n");
+		} else {
+			System.out.println("That's not nice, Tyler...\n\n");
+		}
+	}
+}
